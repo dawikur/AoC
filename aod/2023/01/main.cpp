b438f4afe7269530e9d97b61c991c76fb19cb7e0
Dawid Kurek <dawikur@gmail.com>
2023-12-01 12:28:09 +0100

    ⭐ 2023/01

---
+++ aod/2023/01/main.cpp
@@ -0,0 +1,112 @@
+// Copyright (c) 2023 Dawid Kurek <dawikur@gmail.com>
+
+#include <chrono>
+
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include <aod/config.hpp>
+
+std::vector<std::string> read_file (std::ifstream input)
+{
+	std::vector<std::string> lines;
+	std::string              line;
+	while (std::getline (input, line)) { lines.push_back (line); }
+	return lines;
+}
+
+std::array<std::string_view, 10> digits = {"zero",
+										   "one",
+										   "two",
+										   "three",
+										   "four",
+										   "five",
+										   "six",
+										   "seven",
+										   "eight",
+										   "nine"};
+
+int solve_naive (std::vector<std::string> const& lines)
+{
+	int result = 0;
+
+	for (auto const& line: lines) {
+		{
+			int pos   = std::numeric_limits<int>::max ();
+			int digit = 0;
+
+			for (std::size_t i = 1; i < digits.size (); ++i) {
+				auto const pos_str = line.find (digits [i]);
+				if (pos_str != std::string_view::npos
+					&& static_cast<int> (pos_str) < pos) {
+					pos   = static_cast<int> (pos_str);
+					digit = static_cast<int> (i);
+				}
+			}
+
+			if (auto const pos_digit = line.find_first_of ("0123456789");
+				pos_digit != std::string_view::npos
+				&& static_cast<int> (pos_digit) < pos) {
+				pos   = static_cast<int> (pos_digit);
+				digit = static_cast<std::size_t> (line [pos_digit] - '0');
+			}
+
+			result += digit * 10;
+		}
+
+		{
+			auto pos   = std::numeric_limits<int>::min ();
+			auto digit = 0;
+
+			for (std::size_t i = 1; i < digits.size (); ++i) {
+				auto const pos_str = line.rfind (digits [i]);
+				if (pos_str != std::string_view::npos
+					&& static_cast<int> (pos_str) > pos) {
+					pos   = static_cast<int> (pos_str);
+					digit = static_cast<int> (i);
+				}
+			}
+
+			if (auto const pos_digit = line.find_last_of ("0123456789");
+				pos_digit != std::string_view::npos
+				&& static_cast<int> (pos_digit) > pos) {
+				pos   = static_cast<int> (pos_digit);
+				digit = static_cast<std::size_t> (line [pos_digit] - '0');
+			}
+
+			result += digit;
+		}
+	}
+
+	return result;
+}
+
+int main (int argc, char** argv)
+{
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv [0] << " <input file>" << std::endl;
+		return 1;
+	}
+
+	auto const lines = read_file (std::ifstream {argv [1]});
+
+	std::chrono::steady_clock::time_point const begin
+		= std::chrono::steady_clock::now ();
+	int const                                   solution = solve_naive (lines);
+	std::chrono::steady_clock::time_point const end
+		= std::chrono::steady_clock::now ();
+
+	std::cout << "[Naive] Elapsed time: "
+			  << std::chrono::duration_cast<std::chrono::microseconds> (end
+																		- begin)
+					 .count ()
+			  << "µs" << std::endl;
+
+	std::cout << "Answer: " << solution << std::endl;
+
+	return 0;
+}
---
+++ config.tpp
@@ -0,0 +1,73 @@
+// Copyright (c) 2023-${DATE_YEAR} Dawid Kurek <dawikur@gmail.com>
+
+#pragma once
+
+#include <cstdint>
+
+#include <string_view>
+
+namespace dku {
+
+// static inline constexpr auto const () const noexcept
+
+struct version {
+	static constexpr std::int32_t const major = ${${PROJECT_NAME}_VERSION_MAJOR};
+	static constexpr std::int32_t const minor = ${${PROJECT_NAME}_VERSION_MINOR};
+	static constexpr std::int32_t const patch = 0;
+	static constexpr std::string_view const hash = "${GIT_HASH}";
+
+	static constexpr bool const is_dirty = ${GIT_DIRTY} != 0;
+};
+
+struct config {
+	static constexpr std::string_view const name = "${PROJECT_NAME}";
+
+	static constexpr std::string_view const system_name = "${CMAKE_SYSTEM_NAME}";
+	static constexpr std::string_view const build_name  = "${CMAKE_BUILD_TYPE}";
+	static constexpr std::string_view const compiler_id = "${CMAKE_CXX_COMPILER_ID}";
+
+#if defined(IS_RELEASE)
+	static constexpr bool const is_release = true;
+	static constexpr bool const is_debug = false;
+#elif defined(IS_DEBUG)
+	static constexpr bool const is_release = false;
+	static constexpr bool const is_debug = true;
+#else
+#	error "Unknown build type"
+#endif
+
+#if defined(IS_UNIX)
+	static constexpr bool const is_unix = true;
+	static constexpr bool const is_win32 = false;
+	static constexpr bool const is_macos = false;
+#elif defined(IS_WINDOWS)
+	static constexpr bool const is_unix = false;
+	static constexpr bool const is_win32 = true;
+	static constexpr bool const is_macos = false;
+#elif defined(IS_MACOS)
+	static constexpr bool const is_unix = false;
+	static constexpr bool const is_win32 = false;
+	static constexpr bool const is_macos = true;
+#else
+#   error "Unknown platform"
+#endif
+
+#if defined(IS_CLANG)
+	static constexpr bool const is_clang = true;
+	static constexpr bool const is_gnu = false;
+	static constexpr bool const is_msvc = false;
+#elif defined(IS_GNU)
+	static constexpr bool const is_clang = false;
+	static constexpr bool const is_gnu = true;
+	static constexpr bool const is_msvc = false;
+#elif defined(IS_MSVC)
+	static constexpr bool const is_clang = false;
+	static constexpr bool const is_gnu = false;
+	static constexpr bool const is_msvc = true;
+#else
+#	error "Unknown compiler"
+#endif
+
+};
+
+} // namespace dku
