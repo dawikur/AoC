5554a2727be0a74733e3bdd4bb86824fbeb228cc
Dawid Kurek <dawikur@gmail.com>
2023-12-01 17:56:19 +0100

    ⭐ 2015/01-03

---
+++ include/aod/file.hpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2023 Dawid Kurek <dawikur@gmail.com>
+
+#pragma once
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace aod {
+
+std::vector<std::string> read_file (std::ifstream input)
+{
+	std::vector<std::string> lines;
+	std::string              line;
+	while (std::getline (input, line)) { lines.push_back (line); }
+	return lines;
+}
+
+std::vector<std::string> read_input (std::string const& file)
+{
+	std::filesystem::path const path {file};
+	auto const                  input = path.parent_path () / "input.txt";
+
+	return read_file (std::ifstream {input.string ()});
+}
+
+} // namespace aod
---
+++ src/2015/02/main.cpp
@@ -0,0 +1,52 @@
+// Copyright (c) 2023 Dawid Kurek <dawikur@gmail.com>
+
+#include <aod/file.hpp>
+
+int main ()
+{
+	auto const input = aod::read_input (__FILE__);
+
+	int paper  = 0;
+	int ribbon = 0;
+	for (auto const& line: input) {
+		int l;
+		int w;
+		int h;
+
+		std::sscanf (line.c_str (), "%dx%dx%d", &l, &w, &h);
+
+		{
+			auto const lw = l * w;
+			auto const wh = w * h;
+			auto const hl = h * l;
+
+			paper += 2 * lw + 2 * wh + 2 * hl;
+
+			if (lw < wh && lw < hl) {
+				paper += lw;
+			} else if (wh < hl) {
+				paper += wh;
+			} else {
+				paper += hl;
+			}
+		}
+		{
+			auto const lw = l + w;
+			auto const wh = w + h;
+			auto const hl = h + l;
+
+			if (lw < wh && lw < hl) {
+				ribbon += 2 * lw;
+			} else if (wh < hl) {
+				ribbon += 2 * wh;
+			} else {
+				ribbon += 2 * hl;
+			}
+
+			ribbon += l * w * h;
+		}
+	}
+
+	std::cout << "Paper: " << paper << std::endl;
+	std::cout << "Ribbon: " << ribbon << std::endl;
+}
